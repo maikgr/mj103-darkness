89be2133412f7984f3540c2c1f0d99382cf6365f
maikgr <maikgibranr@gmail.com>
2022-04-21 23:01:14 +0800

    Add flickering light effect

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,29 +6,51 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Light2D light2dController;
+    public float flickerOffset;
+    public Light2D stableLight;
+    public Light2D flickeringLight;
     private LevelGenerator levelGenerator;
+    private float baseOuterRadius;
+    private float baseInnerRadius;
 
     // Start is called before the first frame update
     void Start()
     {
         levelGenerator = GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelGenerator>();
+        baseOuterRadius = stableLight.pointLightOuterRadius + 0;
+        baseInnerRadius = stableLight.pointLightInnerRadius + 0;
+        StartCoroutine(LightFlickerCoroutine());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Movement
         transform.localPosition = new Vector2(
                 transform.localPosition.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed,
                 transform.localPosition.y + Input.GetAxis("Vertical") * Time.deltaTime * speed
             );
+
+        // Light level
         if (Input.GetButton("Fire1"))
         {
-            light2dController.pointLightOuterRadius += Time.deltaTime * 5;
+            stableLight.pointLightOuterRadius += Time.deltaTime * 5;
+            baseOuterRadius = stableLight.pointLightOuterRadius + 0;
         }
         else if (Input.GetButton("Fire2"))
         {
-            light2dController.pointLightOuterRadius -= Time.deltaTime * 5;
+            stableLight.pointLightOuterRadius -= Time.deltaTime * 5;
+            baseOuterRadius = stableLight.pointLightOuterRadius + 0;
+        }
+    }
+
+    private IEnumerator LightFlickerCoroutine() {
+        // Light flickers
+        while(true)
+            {
+            flickeringLight.pointLightOuterRadius = baseOuterRadius - Random.Range(0f, flickerOffset);
+            flickeringLight.pointLightInnerRadius = baseInnerRadius - Random.Range(0f, flickerOffset * 3);
+            yield return new WaitForSeconds(Random.Range(0f, 0.2f));
         }
     }
 
