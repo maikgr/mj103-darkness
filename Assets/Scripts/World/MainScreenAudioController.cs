6ffb90c576f4b1c72ac15070df844a48ec22ba92
maikgr <maikgibranr@gmail.com>
2022-05-02 23:59:01 +0800

    Add level bgm

--- Assets/Scripts/World/MainScreenAudioController.cs
+++ Assets/Scripts/World/MainScreenAudioController.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class MainScreenAudioController : MonoBehaviour
 {
     [SerializeField]
-    private List<AudioClip> AudioClips;
+    private List<AudioClip> startBgm;
     [SerializeField]
-    private AudioSource AudioSource;
+    private AudioSource startBgmSource;
+    [SerializeField]
+    private AudioSource mainAudioBgm;
+    public float mainAudioDucking;
+    private float initialMainBgmVolume;
 
     private void Awake() {
-        var clipIndex = Mathf.Clamp(GameController.Instance.currentLevel, 0, AudioClips.LastIndex());
-        AudioSource.clip = AudioClips[clipIndex];
-        AudioSource.Play();
+        var clipIndex = Mathf.Clamp(GameController.Instance.currentLevel, 0, startBgm.LastIndex());
+        startBgmSource.clip = startBgm[clipIndex];
+        startBgmSource.Play();
+        initialMainBgmVolume = mainAudioBgm.volume;
+    }
+
+    private void Start() {
+        mainAudioBgm.Play();
+    }
+
+    private void Update() {
+        if (!startBgmSource.isPlaying && mainAudioBgm.volume == mainAudioDucking)
+        {
+            mainAudioBgm.volume = initialMainBgmVolume;
+        }
+        else if (startBgmSource.isPlaying && mainAudioBgm.volume != mainAudioDucking)
+        {
+            mainAudioBgm.volume = mainAudioDucking;
+        }
     }
 }
